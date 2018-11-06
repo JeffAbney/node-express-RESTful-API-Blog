85181564c77a3b6a3d88cd2c65f5af1034509c69
JeffAbney <jeffmabney@gmail.com>
2018-11-06 08:58:52 -0600

    Add comments CRU, D function not working.

--- routes/comments.js
+++ routes/comments.js
@@ -1,14 +1,35 @@
 module.exports = {
-  getComments(req, res) {
-    
+
+  getComments: function(app, store) {
+    app.get('/posts/:postId/comments', (req, res) => {
+      res.send(store.posts[req.params.postId.comments])
+      console.log("Getting comments", store.posts[req.params.postId].comments)
+    })
   }, 
-  addComment(req, res) {
-    
+  addComment: function(app,store) {
+    app.post('/posts/:postId/comments', (req, res) => {
+      store.posts[req.params.postId].comments.push(req.body)
+      console.log("Writing comment", store.posts[req.params.postId].comments)
+      res.sendStatus(201)
+    })
   },
-  updateComment(req, res) {
-    
+  updateComment: function(app, store) {
+    app.put('/posts/:postId/comments/:commentId', (req, res) => {
+      let newComment = store.posts[req.params.postId].comments[req.params.commentId]
+      Object.assign
+        (newComment, req.body)
+      console.log("Updating post", store.posts[req.params.postId].comments)
+      res.sendStatus(204)
+    })
   },
-  removeComment(req, res) {
-    
-  }  
+
+  /*NOT WORKING */
+  
+  removeComment: function(app, store) {
+  	app.delete('posts/:postId/comments/:commentId', (req, res) => {
+      store.posts[req.params.postId].comments.splice(req.params.commentId, 1)
+      console.log("Deleting comment", store.posts[req.params.postId].comments)
+      res.sendStatus(204)
+  	})
+  }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,26 +1,26 @@
 module.exports = {
   
-  getPost: function (app, store) {
+  getPost: function(app, store) {
   	app.get('/posts', (req, res) => {
   	  console.log("Getting Posts ", store.posts)
 	  res.send(store.posts)
     })
   },
-  addPost: function (app, store) {
+  addPost: function(app, store) {
   	app.post('/posts', (req, res) => {
   	  store.posts.push(req.body)
   	  console.log("Adding post ", store.posts)
   	  res.sendStatus(201)
     })
   },
-  updatePost: function (app, store) {
+  updatePost: function(app, store) {
   	app.put('/posts/:postId', (req, res) => {
   	  Object.assign(store.posts[req.params.postId], req.body)
   	  console.log("Updating post", store.posts[req.params.postId])
       res.sendStatus(204)
     })
   },
-  removePost: function (app, store) {
+  removePost: function(app, store) {
   	app.delete('/posts/:postID', (req, res) => {
       store.posts.splice(req.params.postId, 1)
       console.log("Deleting Post", store.posts)
